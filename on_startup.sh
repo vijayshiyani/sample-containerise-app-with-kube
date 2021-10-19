2f92ffd08e844d970c7072b7d7ed036232333a17
Vijay Shiyani <vshiyani@meeco.me>
2021-10-19 22:51:37 +1100

    remove old repository code

--- on_startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# do extra stuff e.g. npm run typeorm:migration:run;
-echo $(pwd) 
-echo $(ls) 
-echo $(cat .git/$(cat .git/HEAD | cut -d' ' -f2)) > public/current_build_git_commit.txt;
-
-exec "$@"
--- src/dto/myapplication.dto.ts
+++
@@ -1,7 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { VersionDto } from './version.dto';
-
-export class MyApplicationDto {
-  @ApiProperty({ type: VersionDto })
-  myapplication: VersionDto[];
-}
